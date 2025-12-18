282420f705320855387ae0ca5a229ef545bc2d09
Hamz <ah1188x@gmail.com>
2025-12-18 16:15:32 +0400

    Initial commit

---
+++ DesertSurvivorsTests/DesertSurvivorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesertSurvivorsTests.swift
+//  DesertSurvivorsTests
+//
+//  Created by Ahmed AlHameli on 18/12/2025.
+//
+
+import Testing
+@testable import DesertSurvivors
+
+struct DesertSurvivorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
